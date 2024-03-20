d569e9181ddc7a8e5f14996205afcdb51870ba48
vijay-ram365 <ramkissoon.vijay@gmail.com>
2024-03-20 16:59:50 -0400

    edit loader page

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,8 +2,8 @@ export default function Loader() {
   // this is a presentational component only
   return (
     <div className="bg-slate-900 text-slate-400">
-      <p>Loading Tickets...</p>
-      <p>This might take a minute...</p>
+      <p className="p-2">Loading Tickets...</p>
+      <p className="p-2">This might take a minute...</p>
     </div>
   );
 }
